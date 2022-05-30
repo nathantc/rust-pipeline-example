11a03792262199829ab59deda3648516fee8ba8c
Nathan Carver <nathan@codecarver.io>
2022-05-30 11:06:25 -0400

    Can create an empty post

--- tests/lib-test.rs
+++ tests/lib-test.rs
@@ -1,8 +1,15 @@
 #[cfg(test)]
 mod tests {
+    use rust_pipeline_example::Post;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn can_create_a_new_post() {
+        let mut post = Post::new();
     }
 }
