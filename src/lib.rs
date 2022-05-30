b41e8de6ff3d43944a9fb4c0e96b63c68c8fa602
Nathan Carver <nathan@codecarver.io>
2022-05-30 11:16:28 -0400

    Fixed clippy issues on struct Post

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,8 @@
-pub struct Post {
-    state: Option<Box<dyn State>>,
-    content: String,
-}
+#[derive(Default)]
+pub struct Post {}
 
 impl Post {
     pub fn new() -> Post {
-        Post {
-            state: Some(Box::new(Draft {})),
-            content: String::new(),
-        }
+        Post {}
     }
 }
-
-trait State {}
-
-struct Draft {}
-
-impl State for Draft {}
